8462a2bb3cc4d3b219741f14f4c946bd1d6f9ad6
sourov7 <37473187+sourov7@users.noreply.github.com>
2018-10-17 20:12:57 +0600

    Updates

---
+++ precache-manifest.a2edef0963e59406a1758b2876ba9b96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/hellorobot/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ca570ee3f1ffc2d73027",
+    "url": "/hellorobot/static/js/runtime~main.ca570ee3.js"
+  },
+  {
+    "revision": "4c39b06632b058a4ba1a",
+    "url": "/hellorobot/static/js/main.4c39b066.chunk.js"
+  },
+  {
+    "revision": "07a45d18404d430b0361",
+    "url": "/hellorobot/static/js/1.07a45d18.chunk.js"
+  },
+  {
+    "revision": "4c39b06632b058a4ba1a",
+    "url": "/hellorobot/static/css/main.043fc367.chunk.css"
+  },
+  {
+    "revision": "07a45d18404d430b0361",
+    "url": "/hellorobot/static/css/1.b4e0e737.chunk.css"
+  },
+  {
+    "revision": "9bf97a7ae795214c4a8060ba0d372084",
+    "url": "/hellorobot/index.html"
+  }
+];
